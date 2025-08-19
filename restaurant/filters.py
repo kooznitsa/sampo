168c032f5ef1cf556d94f1cec171a23e625e1db0
Yulia Kuznetsova <kooznitsa@gmail.com>
2025-08-19 18:18:08 +0300

    refactor serializers

--- restaurant/filters.py
+++ restaurant/filters.py
@@ -1,5 +1,19 @@
+from typing import NoReturn
+
 import django_filters
+from rest_framework.exceptions import NotFound
+
+import restaurant.models as models
 
 
 class DishFilterSet(django_filters.FilterSet):
-    restaurant = django_filters.NumberFilter(label='Restaurant ID', field_name='restaurant', lookup_expr='exact')
+    restaurant = django_filters.NumberFilter(method='filter_restaurant')
+
+    class Meta:
+        model = models.Dish
+        fields = ['restaurant']
+
+    def filter_restaurant(self, queryset, name: str, value: int) -> models.Dish | NoReturn:
+        if not models.Restaurant.objects.filter(pk=value).exists():
+            raise NotFound(detail=f'Restaurant with id={value} was not found')
+        return queryset.filter(restaurant_id=value)
