1fb3301eefcf2c3c1d41ab15c2d88f8b053b0738
Yulia Kuznetsova <kooznitsa@gmail.com>
2025-10-14 11:05:10 +0300

    refactor collect links service

--- restaurant/services/__init__.py
+++ restaurant/services/__init__.py
@@ -1,4 +1,3 @@
-from .base_crawler import BaseCrawler  # noqa
 from .driver import DriverManager  # noqa
 from .link_collector import LinkCollector  # noqa
 from .menu_scraper import MenuScraper  # noqa
