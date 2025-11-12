73e52911717f85d9f85dfb84457ee5d4e5a24bc8
Julia Kuznetsova <104004781+kooznitsa@users.noreply.github.com>
2025-11-12 16:58:28 +0300

    Fix Elasticsearch tests and admin (#25)

--- restaurant/documents.py
+++ restaurant/documents.py
@@ -6,7 +6,7 @@ from django_elasticsearch_dsl import Document, fields
 from django_elasticsearch_dsl.registries import registry
 from elasticsearch_dsl import analysis, analyzer
 
-from restaurant.models import Dish, Tag
+import restaurant.models as models
 
 russian_stop = analysis.token_filter('russian_stop', type='stop', stopwords='_russian_')
 russian_stemmer = analysis.token_filter('russian_stemmer', type='stemmer', language='russian')
@@ -41,15 +41,15 @@ class DishDocument(Document):
         }
 
     class Django:
-        model = Dish
-        related_models = [Tag]
+        model = models.Dish
+        related_models = [models.Tag]
         fields = ['id']
 
     def get_instances_from_related(self, related_instance: Any) -> QuerySet | None:
-        if isinstance(related_instance, Tag):
+        if isinstance(related_instance, models.Tag):
             return self.Django.model.objects.filter(tags=related_instance)
         return None
 
-    def prepare_search_text(self, instance: Dish) -> str:
+    def prepare_search_text(self, instance: models.Dish) -> str:
         tag_names = ' '.join(tag.name for tag in instance.tags.all())
         return f'{instance.name} {instance.comment or ""} {tag_names}'
--- restaurant/tests/test_admin.py
+++ restaurant/tests/test_admin.py
@@ -1,29 +1,42 @@
 from django.test import RequestFactory, tag, TestCase
 
+from unittest.mock import patch, MagicMock
+
 from restaurant.admin import DishAdmin
+from restaurant.elastic import DishElasticQueryManager
 import restaurant.models as models
 import restaurant.tests.factories as factories
 
 
 @tag('admin')
 class TestAdmin(TestCase):
-    dish_names = (
-        'котлета по-киевски', 'котлеты по-киевски', 'КОТЛЕТА ПО КИЕВСКИ', 'киевская котлета',
-        'котлета из Киева', 'пожарская котлета', 'котлета с сыром',
-    )
-    search_text = 'котлета по-киевски'
 
     def setUp(self) -> None:
         category = factories.CategoryFactory.create()
         city = factories.CityFactory.create()
         factories.RestaurantFactory.create(category=category, city=city)
 
-    def test_admin_search_uses_elasticsearch(self) -> None:
+        self.dish_names = (
+            'котлета по-киевски', 'котлеты по-киевски', 'КОТЛЕТА ПО КИЕВСКИ', 'киевская котлета',
+            'котлета из Киева', 'пожарская котлета', 'котлета с сыром',
+        )
+        self.search_text = 'котлета по-киевски'
+        self.request_factory = RequestFactory()
+
+    @patch.object(DishElasticQueryManager, 'perform_search')
+    @patch.object(DishElasticQueryManager, 'query_multi_match')
+    def test_admin_search_uses_elasticsearch(self, mock_query_multi: MagicMock, mock_perform_search: MagicMock) -> None:
         for name in self.dish_names:
             factories.DishFactory.create(restaurant=models.Restaurant.objects.first(), name=name)
 
+        num_of_returned_objects = 4
+
+        mock_query_multi.return_value = MagicMock(name='mocked_elastic_query')
+        mock_perform_search.return_value = models.Dish.objects.all()[:num_of_returned_objects]
+
         request = RequestFactory().get('/admin/restaurant/dish/?q=котлета+по-киевски')
         admin = DishAdmin(models.Dish, None)
-        queryset, _ = admin.get_search_results(request, models.Dish.objects.all(), self.search_text)
+        queryset, use_distinct = admin.get_search_results(request, models.Dish.objects.all(), self.search_text)
 
-        self.assertEqual(queryset.count(), 4)
+        self.assertEqual(queryset.count(), num_of_returned_objects)
+        self.assertFalse(use_distinct)
